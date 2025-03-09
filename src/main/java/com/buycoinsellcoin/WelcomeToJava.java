b1828b04ef910f459beb5655ab72de898187ac30
gurunath.sugavanam@gmail.com <guru@dell-g15>
2025-03-09 15:01:02 -0500

    https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true

---
+++ src/main/java/com/buycoinsellcoin/WelcomeToJava.java
@@ -0,0 +1,11 @@
+package com.buycoinsellcoin;
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/welcome-to-java">...</a>
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
